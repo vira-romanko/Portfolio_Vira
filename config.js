98bb6f6009b273e308ac8f17d350eb970bbb1f79
Vira <vira.romanko99@gmail.com>
2021-01-14 18:46:51 -0800

    neeeew

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 const config = {
-  //  host:"us-cdbr-iron-east-05.cleardb.net",
-   host:"localhost",
-  port: 8889, // 8889 for mac
+   host:"us-cdbr-iron-east-05.cleardb.net",
+   //host:"localhost",
+  //port: 8889, // 8889 for mac
    uname:"b50ca0ca526952",
-   uname:"root",
-    pname:"root",
-  // pname:"699abf04", // leave blank for windows
- //  database:"heroku_eab575b93b335ca"
+  // uname:"root",
+  //  pname:"root",
+  pname:"699abf04", // leave blank for windows
+  database:"heroku_eab575b93b335ca"
    //for local environment
-   database:"db_portfolio"
+  // database:"db_portfolio"
 }
 
 module.exports = config;
